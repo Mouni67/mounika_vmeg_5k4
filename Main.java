6758fc555daf10bd04ba86e2c8af11ff8893eb12
G.Mounika <31740673+Mouni67@users.noreply.github.com>
2017-09-08 16:38:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+     
+               Scanner sc=new Scanner(System.in);
+               int id=sc.nextInt();
+               String fullName=sc.next();
+               Date birthDate=sc.next();
+               double avgMark=sc.nextDouble();
+               Student s = new Student(id,fullName,birthDate,avgMark);
+               
+               
+	}
+
+}
